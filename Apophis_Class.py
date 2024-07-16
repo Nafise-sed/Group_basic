0579af5e18cdd1ddf7f7787f22905ec121765d3e
lambertra <lambertra1@gmail.com>
2024-07-16 11:58:48 -0700

    Finished init function and added starting postion

--- Apophis_Class.py
+++ Apophis_Class.py
@@ -1,8 +1,24 @@
 import numpy as np
 from astropy import constants as c
+from astropy import units as u
 
 class Apophis(object):
 
-	def __init__(self, G=c.G, apo_mass=2.699e10,sol_mass=c.M_sun):
+	def __init__(self, G=c.G, apo_mass=2.699e10*u.kg,sol_mass=c.M_sun,
+			     v = 34.44*u.km/u.s, n = 365 ):
+		# Editable parameters
 		self.G = G
-		#TODO save other variables that are given to the class
+		self.a_mass =  apo_mass
+		self.sol_mass = sol_mass
+		self.v = v
+		self.n = n
+
+		# Apophis starting position
+		self.x = 0.75 * c.au.to(u.km)
+		self.y = 0
+	
+	def get_position(self):
+		pass
+		#TODO get_position function
+
+	
