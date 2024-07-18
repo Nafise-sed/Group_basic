69743bc3ffce064c3b2327ccf94bd22de3f81911
Utkarsh Bhura <utkarshbhura@Utkarshs-MacBook-Air.local>
2024-07-18 13:45:33 +0530

    Initiating Plotting Script

---
+++ Plotting.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Apophis_Class import Apophis as A
+from matplotlib.animation import FuncAnimation
+
+temp = A()
+
+print(temp.get_position())
+
